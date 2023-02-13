77030c6dacf207c80e66c0bcab9cc487c93f0e04
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-02-13 00:35:41 +0000

    BankAccount class that should display transactions, deposit given amount to account

--- bank_account.js
+++ bank_account.js
@@ -10,6 +10,10 @@ class BankAccount {
     constructor() {
         this.transactions = [];
     }
+    deposit(amount, date) {
+        let balance = this.transactions.length > 0 ? this.transactions[this.transactions.length - 1].balance + amount : amount;
+        this.transactions.push(new Transaction(date, amount, balance));
+    }
 }
 
 module.exports = {
