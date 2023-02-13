2577b9c355b4803f2d6bce511c5fddd94780b046
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-02-13 02:03:33 +0000

    TDD BankAccount class that should display transactions, deposit a given amount to account,  withdraw a given amount from the account and display account's statement

--- bank_account.js
+++ bank_account.js
@@ -18,8 +18,26 @@ class BankAccount {
         let balance = this.transactions[this.transactions.length - 1].balance - amount;
         this.transactions.push(new Transaction(date, -amount, balance));
     }
+    statement() {
+        console.log("date || credit || debit || balance");
+        for (let i = this.transactions.length - 1; i >= 0; i--) {
+            let credit = this.transactions[i].amount >= 0 ? this.transactions[i].amount.toFixed(2) : "";
+            let debit = this.transactions[i].amount < 0 ? (-this.transactions[i].amount).toFixed(2) : "";
+            let balance = this.transactions[i].balance.toFixed(2);
+            console.log(`${this.transactions[i].date} || ${credit} || ${debit} || ${balance}`);
+        }
+    }
 
 }
+
+const account = new BankAccount();
+account.deposit(1000, "10-01-2023");
+account.deposit(2000, "13-01-2023");
+account.withdrawal(500, "14-01-2023");
+account.statement();
+
+const date = new Date("10-01-2023");
+const originalDate = Date;
 
 module.exports = {
     Transaction,
--- bank_account.test.js
+++ bank_account.test.js
@@ -1,7 +1,7 @@
 const Transaction = require('./bank_account').Transaction;
 const BankAccount = require('./bank_account').BankAccount;
 
-describe("Transaction", () => {
+describe("Transaction class", () => {
     it("should display the date", () => {
         const date = '10-01-2023';
         const transaction = new Transaction(date, 100, 100);
@@ -19,7 +19,7 @@ describe("Transaction", () => {
     });
 });
 
-describe("BankAccount", () => {
+describe("BankAccount class", () => {
     let account;
   
     beforeEach(() => {
@@ -36,8 +36,25 @@ describe("BankAccount", () => {
     });
     it("should withdraw a given amount from the account", () => {
         account.deposit(1000, "10-01-2023");
-        account.withdrawal(500, "11-01-2023");
+        account.withdrawal(500, "14-01-2023");
         expect(account.transactions).toHaveLength(2);
         expect(account.transactions[1].amount).toBe(-500);
+    });
+    it("displays the date, credit, debit and balance of the account on a statement", () => {
+
+        account.deposit(1000, "10-01-2023");
+        account.deposit(2000, "13-01-2023");
+        account.withdrawal(500, "14-01-2023");
+
+        const statementSpy = jest.spyOn(console, "log").mockImplementation();
+    
+        account.statement();
+    
+        expect(statementSpy).toHaveBeenCalledWith("date || credit || debit || balance");
+        expect(statementSpy).toHaveBeenCalledWith("14-01-2023 ||  || 500.00 || 2500.00");
+        expect(statementSpy).toHaveBeenCalledWith("13-01-2023 || 2000.00 ||  || 3000.00");
+        expect(statementSpy).toHaveBeenCalledWith("10-01-2023 || 1000.00 ||  || 1000.00");
+    
+        statementSpy.mockRestore();
     });
 });
